e2662325561f93074e74a1aa8d3230b963577d19
AliNajafi <AliNajafiTabriz@gmail.com>
2020-10-11 21:25:11 +0330

    Add global exception handler

---
+++ GlobalExceptionHandler/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function handleMissedException(Exception $e) {
+    echo "Sorry! Something went wrong!";
+    $message = "Fatal error with this message: " . $e->getMessage() . " occurred in file: "
+        . $e->getFile() . " on line: " . $e->getLine();
+    error_log($message);
+}
+
+set_exception_handler('handleMissedException');
+
+throw new Exception('Just for test!');
